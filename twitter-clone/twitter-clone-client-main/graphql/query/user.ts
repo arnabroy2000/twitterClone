d8307f5144b8c16251466e0018571480d06bb15d
Arnab Roy <royarnab056@gmail.com>
2023-09-12 01:18:01 +0530

    Twitter Clone

---
+++ twitter-clone/twitter-clone-client-main/graphql/query/user.ts
@@ -0,0 +1,51 @@
+import { graphql } from '../../gql';
+
+export const verifyUserGoogleTokenQuery = graphql(`
+  #graphql
+  query VerifyUserGoogleToken($token: String!) {
+    verifyGoogleToken(token: $token)
+  }
+`);
+
+export const getCurrentUserQuery = graphql(`
+  #graphql
+  query GetCurrentUser {
+    getCurrentUser {
+      id
+      profileImageURL
+      email
+      firstName
+      lastName
+      tweets {
+        id
+        content
+        author {
+          firstName
+          lastName
+          profileImageURL
+        }
+      }
+    }
+  }
+`);
+
+export const getUserByIdQuery = graphql(`
+  #graphql
+
+  query GetUserById($id: ID!) {
+    getUserById(id: $id) {
+      id
+      firstName
+      lastName
+      tweets {
+        content
+        id
+        author {
+          firstName
+          lastName
+          profileImageURL
+        }
+      }
+    }
+  }
+`);
